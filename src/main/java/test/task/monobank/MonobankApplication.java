b81236e480dff1cb9b49f85d24141f03a07f2bee
Allduiin <vovapro01@gmail.com>
2021-02-09 12:04:35 +0200

    start commit

---
+++ src/main/java/test/task/monobank/MonobankApplication.java
@@ -0,0 +1,13 @@
+package test.task.monobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonobankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonobankApplication.class, args);
+    }
+
+}
