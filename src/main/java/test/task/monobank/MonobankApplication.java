dc54ad01302a3b7aa77f8e384f93b14931836810
Allduiin <vovapro01@gmail.com>
2021-02-10 15:50:30 +0200

    Added getStatus function And starting add  ActualStatusTakeService

--- src/main/java/test/task/monobank/MonobankApplication.java
+++ src/main/java/test/task/monobank/MonobankApplication.java
@@ -2,12 +2,14 @@ package test.task.monobank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import static test.task.monobank.model.Request.Status.PROCESSING;
 
 @SpringBootApplication
 public class MonobankApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MonobankApplication.class, args);
+        System.out.println(PROCESSING.toString());
     }
 
 }
---
+++ src/main/java/test/task/monobank/service/actual_status_take/impl/ActualStatusTakeServiceImpl.java
@@ -0,0 +1,13 @@
+package test.task.monobank.service.actual_status_take.impl;
+
+import java.util.List;
+import test.task.monobank.model.Request.Status;
+import test.task.monobank.service.actual_status_take.ActualStatusTakeService;
+
+public class ActualStatusTakeServiceImpl implements ActualStatusTakeService {
+    @Override
+    public Status getActualStatus(List<Status> statuses) {
+
+        return null;
+    }
+}
