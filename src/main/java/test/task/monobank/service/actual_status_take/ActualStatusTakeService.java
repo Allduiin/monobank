68b822d8cfb685c13f63b00c916fae6973cb59ce
Allduiin <vovapro01@gmail.com>
2021-02-10 16:38:54 +0200

    Added working StatusController

--- src/main/java/test/task/monobank/service/actual_status_take/ActualStatusTakeService.java
+++
@@ -1,8 +0,0 @@
-package test.task.monobank.service.actual_status_take;
-
-import java.util.List;
-import test.task.monobank.model.Request.Status;
-
-public interface ActualStatusTakeService {
-    Status getActualStatus(List<Status> statuses);
-}
